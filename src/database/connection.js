341dd94ab5a4a5160bc902e9ce483f869c1961bd
AndresSanchez <sanchezandres8203@gmail.com>
2025-12-09 15:46:57 -0400

    deploy: test

--- src/database/connection.js
+++ src/database/connection.js
@@ -26,13 +26,13 @@ const query = async (text, params) => {
   const start = Date.now();
   const res = await pool.query(text, params);
   const duration = Date.now() - start;
-  // if (process.env.NODE_ENV !== "production") {
+  if (process.env.NODE_ENV !== "production") {
     console.log("Query ejecutada", {
       text: text.substring(0, 50),
       duration,
       rows: res.rowCount,
     });
-  // }
+  }
   return res;
 };
 
