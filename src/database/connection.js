53f3c57b853ee2cc8da2b3d701c9171cad4f9642
AndresSanchez <sanchezandres8203@gmail.com>
2025-12-09 15:34:09 -0400

    deploy: secrets

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,7 +3,11 @@ require("dotenv").config();
 
 // Configuración del pool de conexiones PostgreSQL
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
   ssl: { rejectUnauthorized: false } 
 });
 
